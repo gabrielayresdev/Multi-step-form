bb2226f4614ccb0725e47683f99aa8cfe8c8dd04
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-12-20 17:06:11 -0300

    initial commit

---
+++ plopfile.js
@@ -0,0 +1,24 @@
+export default function (/** @type {import('plop').NodePlopAPI} */ plop) {
+  plop.setGenerator("component", {
+    description: "this is a skeleton for components",
+    prompts: [{ type: "input", name: "name", message: "Name your component" }], // array of inquirer prompts
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{titleCase name}}/Index.tsx",
+        templateFile: "plop/templates/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{titleCase name}}/{{titleCase name}}.module.sass",
+        templateFile: "plop/templates/styles.module.sass.hbs",
+      },
+    ],
+  });
+
+  plop.setHelper("titleCase", (str) => {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  });
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.sass";
+import Test from "./components/test";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
