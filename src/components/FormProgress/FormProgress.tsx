2998ad0bc90fc5cbcdc792e40a0dfc76062bc9a1
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2024-01-06 14:24:09 -0300

    fix: user can't go from first page to another with invalid inputs using pagination controls

--- src/components/FormProgress/FormProgress.tsx
+++ src/components/FormProgress/FormProgress.tsx
@@ -1,3 +1,4 @@
+import { useFormContext } from "../../contexts/FormContext";
 import FormStep from "../FormStep/FormStep";
 import styles from "./FormProgress.module.sass";
 
@@ -7,20 +8,41 @@ interface FormProgressInterface {
 }
 
 export const FormProgress = ({ page, goTo }: FormProgressInterface) => {
+  const { name, email, number } = useFormContext();
+
+  function changePage(page: number) {
+    if (page === 0) {
+      const fields = [name, email, number];
+      const valid = fields.reduce(
+        (bool, current) => current.validate() && bool,
+        true
+      );
+      if (valid) goTo(page);
+    }
+  }
+
   return (
     <div className={styles.formProgress}>
       <FormStep
         step={{ order: 0, title: "your info" }}
         page={page}
-        goTo={goTo}
+        goTo={() => changePage(0)}
       />
       <FormStep
         step={{ order: 1, title: "select plan" }}
         page={page}
-        goTo={goTo}
+        goTo={() => changePage(2)}
       />
-      <FormStep step={{ order: 2, title: "add-ons" }} page={page} goTo={goTo} />
-      <FormStep step={{ order: 3, title: "summary" }} page={page} goTo={goTo} />
+      <FormStep
+        step={{ order: 2, title: "add-ons" }}
+        page={page}
+        goTo={() => changePage(3)}
+      />
+      <FormStep
+        step={{ order: 3, title: "summary" }}
+        page={page}
+        goTo={() => changePage(4)}
+      />
     </div>
   );
 };
